121b42ede06f6d9496413bceeda3b8335501721c
Avinash Malhotra <avimalhotra505@gmail.com>
2020-07-15 17:57:33 +0530

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,20 +1,29 @@
-const express=require("express");
-const app=express();
-const path=require('path');
-const ejs=require('ejs');
+const express = require('express');
+const app = express();
+const path = require("path");
+const ejs = require('ejs');
+const LRU = require('lru-cache');
+//ejs.cache =new LRU(100);  
+//ejs.cache =new LRU(300);  
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/views'));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.use(express.static(path.resolve(__dirname,'public')))
+app.get('/', (req, res) => {
+    //res.send("hi");
+    res.status(200).render('index', { name: "avi", id: 212, isAvailable: false, month: ["Jan","Feb","Mar","Apr","May","June"], person:{ name:"person",age:22,empid:212 } })
+});
 
-
-app.get('/',(req,res)=>{
-    res.render('index',{name:"avi", id:212});
-    //res.status(200).send("hello ejs");
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{ getUser:()=>{ return "ABCD" } })
+});
+app.get('/blogs', (req, res) => {
+    res.status(200).render('blog',{})
 });
 
 
-app.listen(3000,()=>{
-    console.log(`app running at http://127.0.0.1:3000`);
+app.listen(3000, () => {
+    console.log("app runing at http://localhost:3000");
 })
